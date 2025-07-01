d899cc7bb7786bdbc14d648fa0b4742a743f117b
Erick Jesus <erickontiveros017@gmail.com>
2025-07-01 16:23:20 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/erickjesus/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.erickjesus.workshopmongo.repository;
+
+import com.erickjesus.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
