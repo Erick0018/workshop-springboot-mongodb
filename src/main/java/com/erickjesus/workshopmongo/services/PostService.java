37962845d2cd2d6abc4b4ef0c25494bb3d6af5f7
Erick Jesus <erickontiveros017@gmail.com>
2025-07-16 15:16:34 -0300

    Consulta simples com query methods

--- src/main/java/com/erickjesus/workshopmongo/services/PostService.java
+++ src/main/java/com/erickjesus/workshopmongo/services/PostService.java
@@ -6,6 +6,8 @@ import com.erickjesus.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostService {
 
@@ -15,5 +17,9 @@ public class PostService {
     public Post findById(String id) {
         return repository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Id não encontrado!"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repository.findByTitleContaining(text);
     }
 }
